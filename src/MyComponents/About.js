55fd47b2394757b8a2a6d0d20052085d28a0ada4
Ambaliya Dhruv Dharmendrabhai <ambaliyadhruv123@gmail.com>
2024-06-07 20:12:42 +0530

    Add files via upload

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div className='container my-3'>
+        <h3>About us</h3>
+        <p>This webapp is developed by Dhruv Ambaliya and its project to learn react.</p>
+    </div>
+  )
+}
---
+++ src/MyComponents/Todos.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import {TodoItem} from "../MyComponents/TodoItem";
+
+
+export const Todos = (props) => {
+  let myStyle={
+    minhight: "70vh",
+    margin: "10px auto"
+  }
+  
+  return (
+    <div className='container my-3' style={myStyle}>
+        <h3 className='my-3'> Todo List</h3>
+        {props.todos.length===0? "No todos to display":
+        props.todos.map((todo)=>{
+          return (
+          <>
+            <TodoItem todo={todo} key={todo.sno} onDelete={props.onDelete}/> <hr/>  
+          </> )      
+        })}
+    </div>
+  )
+}
